a30dd2b079e05b19276686dd9aebcc09ac280f8b
Kevin Anthony da Cruz Riquena <riquena.969@gmail.com>
2018-08-15 20:18:20 -0300

    Finalizado código para enviar o usuário para página do jogo

--- alzheimer/src/app/app.component.ts
+++ alzheimer/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { PalavrasPage } from '../pages/palavras/palavras';
+
 @Component({
   templateUrl: 'app.html'
 })
--- alzheimer/src/pages/home/home.ts
+++ alzheimer/src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PalavrasPage } from '../palavras/palavras';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  pushPage: any;
   constructor(public navCtrl: NavController) {
-
+	this.pushPage = PalavrasPage;
   }
 
 }
