50bd1a37b19c05e46d4cf4758fb616a762f1860f
Kevin Anthony da Cruz Riquena <riquena.969@gmail.com>
2018-08-15 22:24:13 -0300

    Iniciado desenvolvimento do jogo das palavras

--- alzheimer/src/pages/palavras/palavras.ts
+++ alzheimer/src/pages/palavras/palavras.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PalavrasPage page.
@@ -8,18 +8,82 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-palavras',
-  templateUrl: 'palavras.html',
-})
-export class PalavrasPage {
+ @IonicPage()
+ @Component({
+    selector: 'page-palavras',
+    templateUrl: 'palavras.html',
+ })
+ export class PalavrasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    private palavras;
+    public  palavraMatriz;
+    private palavrasDigitadas;
+    public  palavraDigitada;
+    public  tempoRestante;
+    private timer;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PalavrasPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+        this.palavras          = ['casa', 'mesa', 'gelo', 'blusa', 'jogo', 'bola'];
+        this.palavrasDigitadas = [];
 
-}
+        this.palavraMatriz = this.palavras[Math.floor((Math.random() * this.palavras.length))].toUpperCase();
+        this.tempoRestante = 60;
+
+        this.timer = setInterval(
+            () => {
+                this.tempoRestante--;
+                if (this.tempoRestante == 0) {
+                    clearInterval(this.timer);
+                    this.gameOver();
+                }
+            }, 1000);
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad PalavrasPage');
+    }
+
+    public adicionarPalavra() {
+        if (this.palavraDigitada.toLowerCase().indexOf(this.palavraMatriz.charAt(0).toLowerCase()) == 0) {
+            this.palavrasDigitadas.push({
+                palavra:   this.palavraDigitada,
+                pontuacao: this.calculaPontos(this.palavraDigitada)
+            });
+            this.palavraDigitada = '';
+        }
+    }
+
+    private calculaPontos(palavra) {
+        let pontuacao                     = 0;
+        let valorPorLetraPalavraPrincipal = 100 / this.palavraMatriz.length;
+        let pontoParada                   = palavra.length;
+
+        for (let i = 0 ; i < this.palavraMatriz.length ; i++) {
+            if (palavra.charAt(i).toLowerCase() == this.palavraMatriz.charAt(i).toLowerCase()) {
+                pontuacao += valorPorLetraPalavraPrincipal;
+            } else {
+                break;
+            }
+            pontoParada = i;
+        }
+
+        return pontuacao + (10 * (palavra.length - pontoParada - 1));
+    }
+
+    private gameOver() {
+        let pontuacao;
+
+        pontuacao = this.palavrasDigitadas.reduce(
+            (a, b) => {return {pontuacao: a.pontuacao + b.pontuacao}}
+        );
+
+        const alert = this.alertCtrl.create({
+          title: 'Tempo esgotado!',
+          subTitle: `Você conseguiu ${pontuacao.pontuacao} pontos!`,
+          buttons: ['OK']
+        });
+
+        alert.present();
+    }
+
+ }
